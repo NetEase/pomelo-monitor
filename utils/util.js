f8679fd5d43e91a4d37d6e00dcebf59f56420dc7
lwj <lwj19861218@163.com>
2012-08-16 19:57:50 +0800

    code refactored

--- utils/util.js
+++ utils/util.js
@@ -1,12 +1,12 @@
 /**
- * convert Date as  yyyy-mm-dd
+ * convert Date as  yyyy-mm-dd hh:mm:ss
  */
-function formatTime(date){
-	var n=date.getFullYear(); 
-	var y=date.getMonth();
-	var r=date.getDate(); 
-	var mytime=date.toLocaleTimeString(); 
-	var mytimes= n+ "-" + y + "-" + r + " " + mytime;
-    return mytimes;
+function formatTime(date) {
+	var n = date.getFullYear(); 
+	var y = date.getMonth();
+	var r = date.getDate(); 
+	var mytime = date.toLocaleTimeString(); 
+	var mytimes = n+ "-" + y + "-" + r + " " + mytime;
+  return mytimes;
 }
-module.exports.formatTime=formatTime;
+module.exports.formatTime = formatTime;
