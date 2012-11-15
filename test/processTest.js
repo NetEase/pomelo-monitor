085eb441c02810a3e40498cc6a8209c07bc992b6
lwj <lwj19861218@163.com>
2012-11-15 14:29:24 +0800

    fix bug

--- test/processTest.js
+++ test/processTest.js
@@ -3,11 +3,13 @@ var processMonitor = require('../lib/processMonitor');
 function test() {
 	var param = {
 		pid: process.pid,
-		serverId: 'node-1'
+		serverId: 'connector-server-1'
 	};
 	processMonitor.getPsInfo(param, function(data) {
 		console.log('process information is :', data);
 	});
 };
 
-test();
+for (var i = 0; i < 20; i ++) {
+	test();
+}
