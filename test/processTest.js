88aae7b391e68005023fb3ce70c0e7663446c532
lwj <lwj19861218@163.com>
2012-11-20 18:52:03 +0800

    change cmd

--- test/processTest.js
+++ test/processTest.js
@@ -2,14 +2,15 @@ var processMonitor = require('../lib/processMonitor');
 
 function test() {
 	var param = {
-		pid: process.pid,
-		serverId: 'connector-server-1'
+		pid: 4838,
+		serverId: 'auth-server-1'
 	};
-	processMonitor.getPsInfo(param, function(data) {
+	processMonitor.getPsInfo(param, function(err, data) {
+		if (!!err) {
+			console.log (err);
+			return;
+		}
 		console.log('process information is :', data);
 	});
 };
-
-for (var i = 0; i < 20; i ++) {
-	test();
-}
+test();
